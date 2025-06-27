d7bdaf9c9a7865afc14e8efbbc889a273b709ac3
LukaRakic00 <ltc.it.sektor@gmail.com>
2025-06-27 16:27:51 +0200

    reseno sve oko favico.ico

---
+++ src/app/prijava/layout.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "ikonice/human-resources.svg", // ili "/favicon.ico" ako koristiš .ico
+  },
+};
+
+export default function PrijavaLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+} 
